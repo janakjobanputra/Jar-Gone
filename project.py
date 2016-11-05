f67f163aac77eece6663611dc04bbacb5e8ecc81
Trish Whetzel <plwhetzel@gmail.com>
2016-11-05 16:03:38 -0700

    main file

---
+++ project.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+# Home page
+@app.route('/')
+def home():
+    message = 'Welcome to Jar-Gone'
+    return render_template('home.html', message=message)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
