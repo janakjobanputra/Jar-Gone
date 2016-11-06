3f30de44731432620510b91d2af4e21a29c4973f
Michaela Gobron <michaelagobron@gmail.com>
2016-11-06 01:16:26 -0700

    new annotations.py

--- scripts/annotations.py
+++ scripts/annotations.py
@@ -1,45 +1,62 @@
 import requests
 import xml.etree.ElementTree as ET
+import json
 
 
 # Find definitions for text
-def get_definitions():
+def get_definitions(text_to_annotate):
     annotations = []
+    annotation_dict = {}
 
     dictionary_api = "http://www.dictionaryapi.com/api/references/medical/v2/xml/"
     apikey = "?key=34d82344-06fc-4e6f-baf0-8e6811eb2f18"
 
-    text_to_annotate = "blah melanoma test"
+    stop_words = ['the', 'a', 'as', 'at', 'no', 'in', 'are', 'to', 'for', 'but', 'also', 'index', 'with', 'nuclei']
+    # text_to_annotate = "blah melanoma test"
     words = text_to_annotate.split()
-    print words
+    # print words
 
-    # Annotate each word
+    # Annotate each word if not a stop word
     for word in words:
         print "** "+word+" **"
-        r = requests.get(dictionary_api+word+apikey)
-        print type(r)
+        formatted_word = word.replace(",", "").replace(".", "") .lower()
+        print "** "+formatted_word+" **"
 
-        if r.status_code == 200:
-            response = r.text
-            # print response
+        if not formatted_word in stop_words:
+            r = requests.get(dictionary_api+formatted_word+apikey)
 
-            response_string = response.encode('utf-8')
-            root = ET.fromstring(response_string);
-            print root.tag
-            print root.attrib
+            if r.status_code == 200:
+                response = r.text
+                # print response
 
-            # print root['entry_list']
+                response_string = response.encode('utf-8')
+                root = ET.fromstring(response_string);
+                # print root.tag
+                # print root.attrib
 
-            for child in root.findall('entry'):
-                key = child.get('id')
-                value = child.find('def/sensb/sens/dt').text
-                print (key, value);
+                for child in root.findall('entry'):
+                    key = child.get('id')
+                    value = child.find('def/sensb/sens/dt').text
+                    print (key, value)
 
-            # print root.find('entry_list/entry/def/sensb/sens/dt')
+                    annotation_dict[key] = value
+                # print annotation_dict
+
+    annotations.append(annotation_dict)
+    print json.dumps(annotations)
 
 
-
+# TEST - open file
+def open_file():
+    with open('medical_consult.txt') as data_file:
+        lines = data_file.read()
+        print "Lines: ", lines
+        data_file.close()
+        return lines
 
 # Main
 if __name__ == '__main__':
-    get_definitions()
+    text_to_annotate = open_file()
+    the_annotations = get_definitions(text_to_annotate)
+    # print the_annotations
+
